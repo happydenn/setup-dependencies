b322041f763f4fbea79a7a7dbfe66aba33569205
Denny Tsai <denny.tsai@linecorp.com>
2017-08-06 11:34:20 +0800

    Add -y to apt-get install

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from subprocess import check_call
 class PostDevelopCommand(develop):
     def run(self):
         check_call('apt-get update'.split())
-        check_call('apt-get install gnome-common'.split())
+        check_call('apt-get install -y gnome-common'.split())
         develop.run(self)
 
 class PostInstallCommand(install):
     def run(self):
         check_call('apt-get update'.split())
-        check_call('apt-get install gnome-common'.split())
+        check_call('apt-get install -y gnome-common'.split())
         install.run(self)
 
 setup(
